e9a3ff9ef68ceb8eb7773a50916cae5bd228dd96
havinesh <55496048+havinesh@users.noreply.github.com>
2024-03-19 13:04:50 +0530

    added todo apis

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const todos = require("./db");
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type("html").send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
@@ -54,8 +57,67 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Hello User!
     </section>
   </body>
 </html>
-`
+`;
+
+app.use(express.json());
+
+app.get("/api/todos", (req, res) => {
+  res.json(todos);
+});
+
+// Route to get a specific todo item by ID
+app.get("/api/todos/:id", (req, res) => {
+  const todoId = parseInt(req.params.id);
+  const todo = todos.find((todo) => todo.id === todoId);
+  if (!todo) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+  res.json(todo);
+});
+
+// Route to create a new todo item
+app.post("/api/todos", (req, res) => {
+  const { title, checked } = req.body;
+  if (!title) {
+    return res.status(400).json({ error: "Title is required" });
+  }
+  const newTodo = {
+    id: todos.length + 1,
+    title,
+    checked: checked || false,
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+// Route to update a todo item by ID
+app.put("/api/todos/:id", (req, res) => {
+  const todoId = parseInt(req.params.id);
+  const todo = todos.find((todo) => todo.id === todoId);
+  if (!todo) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+  const { title, checked } = req.body;
+  if (title) {
+    todo.title = title;
+  }
+  if (checked !== undefined) {
+    todo.checked = checked;
+  }
+  res.json(todo);
+});
+
+// Route to delete a todo item by ID
+app.delete("/api/todos/:id", (req, res) => {
+  const todoId = parseInt(req.params.id);
+  const index = todos.findIndex((todo) => todo.id === todoId);
+  if (index === -1) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+  todos.splice(index, 1);
+  res.status(204).send();
+});
---
+++ db.js
@@ -0,0 +1,6 @@
+let todos = [
+  { id: 1, title: "Create a todo app", checked: false },
+  { id: 2, title: "Integrate the apis", checked: true },
+];
+
+module.exports = todos;
